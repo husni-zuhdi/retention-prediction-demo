4dce5fddbad61948ac89e1f31867b450b574562e
husnizuhdi <husni.zuhdi@tokopedia.com>
2022-06-06 21:26:12 +0700

    Update flask api and README till in good condition

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# retention-prediction-demo
-Retentio Prediction for bangkit 2022 demo 
+# Retention Prediction for CC-25
+> Part of Bangkit 2022 🇮🇩🇮🇩🇮🇩
+
+## Stack used
+1. 🤖 Tensorflow 2.8
+2. 🍾 Flask 2.10
+
+## Google Cloud Service used
+1. ⚡️ Cloud Run
+
+## Local requirements
+1. 🐍 Python 3
+2. 🐳 Docker
+3. 🚦 Any IDE you like
+
+## 👨🏽‍💻 How to use in local
+### 📒 Notebook
+1. Clone this repositroy
+2. Run `python install -r requirements.txt`
+3. Open the `retention-prediction.ipynb`
+
+### 📒 Notebook
+1. Clone this repositroy
+2. Open flask folder
+3. Run `python install -r requirements.txt`
+4. Create `.env` file in root folder with this specifications
+
+| Variables             | Function                          | Example       |
+| --------------------- |:--------------------------------: | -------------:|
+| PORT                  | your_API_port                     | 8081          |
+| MODEL_LAYER           | your_model_layer_path             | model.json    |
+| MODEL_WEIGHTS         | your_model_weights_path           | weights.h5    |
+| STANDARD_SCALER       | your_standard_scaler_path (opt)   | sc.pkl        |
+
+5. Run `docker build -t retention-pred:v1.0 .`
+4. Run `export PORT=your_API_port` change to match API port
+4. Run `docker run --name retention-prediction -p 0.0.0.0:$PORT:$PORT -d retention-pred:v1.0`
+5. Test with Postman or CLI
+
+## ☁️ How to use in Cloud
+### ⚡️ Deploy MySQL Database in Compute Engine
+2. Run `export PROJECT_ID=$(gcloud config get-value project)`
+3. Run `docker build -t gcr.io/$PROJECT_ID/retention-pred:v1.0 .`
+4. Run `gcloud auth configure-docker` and press `Y`
+5. Run `docker push gcr.io/$PROJECT_ID/retention-pred:v1.0`
+6. Run `gcloud run deploy --image gcr.io/$PROJECT_ID/retention-pred:v1.0 --platform managed`
+7. Check your GCP Console > Cloud Run
+
+References : 
+
+[1] https://medium.com/@rahulguptalive/create-crud-apis-in-nodejs-express-and-mysql-abda4dfc2d6
+
+[2] https://www.section.io/engineering-education/how-to-build-authentication-api-with-jwt-token-in-nodejs/
+
+[3] https://stackoverflow.com/questions/40076638/compare-passwords-bcryptjs
+
+[4] https://nodejs.org/en/docs/guides/nodejs-docker-webapp/
--- flask/main.py
+++ flask/main.py
@@ -1,11 +1,9 @@
 import os
-import json
-import pickle
 import numpy as np
 from crypt import methods
 from dotenv import load_dotenv
 from flask import Flask, request
-from tensorflow.python.keras.models import model_from_json
+from util import transform, load_model
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -16,57 +14,21 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
-    return "Hello {}!".format(name)
+    return "Hello Bambang {}!".format(name)
 
 @app.route("/predict", methods = ["POST"])
 def predict():
-    data = request.get_json()
-    if data["department"] == "sales":
-         data["department_sales"] = 1
-    elif data["department"] == "accounting":
-        data["department_accounting"] = 1
-    elif data["department"] == "hr":
-        data["department_hr"] = 1
-    elif data["department"] == "technical":
-        data["department_technical"] = 1
-    elif data["department"] == "support":
-        data["department_support"] = 1
-    elif data["department"] == "management":
-        data["department_management"] = 1
-    elif data["department"] == "IT":
-        data["department_IT"] = 1
-    elif data["department"] == "product_mng":
-        data["department_product_mng"] = 1
-    elif data["department"] == "marketing":
-        data["department_marketing"] = 1
-    del data["department"]
+    # transofrm data
+    predict_data = transform(request.get_json())
 
-    if data["salary_level"] == "low":
-         data["salary_level_low"] = 1
-    elif data["salary_level"] == "medium":
-        data["salary_level_medium"] = 1
-    elif data["salary_level"] == "high":
-        data["salary_level_high"] = 1
-    del data["salary_level"]
+    # load model and standard_scaler
+    model, sc = load_model()
 
-    print(data["id"])
-
-    # loading model
-    model = model_from_json(open(os.environ.get("MODEL_LAYER")).read())
-    model.load_weights(os.environ.get("MODEL_WEIGHTS"))
-
-    # dont forget to compile your model
-    model.compile(loss='binary_crossentropy', optimizer='adam')
-
-    # and load standard_scaler
-    with open(os.environ.get("STANDARD_SCALER"), 'rb') as handler:
-        sc = pickle.load(handler)
-
-    # model_pred = model.predict(sc.transform(np.array([data])))
-    model_pred = {"hello": data}
+    # do the prediction
+    result = model.predict(sc.transform(np.array([predict_data])))
+    model_pred = {"left": bool(result > 0.5)}
     
     return model_pred
 
-
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8081)))
